4e188a6ef909eccc5960dfc99f8f594a844833db
lalala <99693356+zyy98123@users.noreply.github.com>
2024-01-29 10:22:19 +0100

    关于监听和命令回复
    基于smtpd库和简单描述一下需要对各种命令的回复

--- server.py
+++ server.py
@@ -20,3 +20,16 @@ class SMTPserver(asyncore.dispatcher):
 
         # 处理邮件数据
         print(f"Message data:\n{data.decode('utf-8')}")
+
+
+if __name__ == '__main__':
+    # 在本地监听 1025 端口
+    server = MySMTPServer(('localhost', 1025), None)
+    print("SMTP Server listening on port 1025...")
+
+    try:
+        #启动异步事件循环。在这个循环中，程序会一直等待和处理事件，包括接收新的SMTP连接、接收数据等
+        asyncore.loop()
+    except KeyboardInterrupt:          #捕获键盘中断异常，即用户按下Ctrl+C中断程序的情况。
+        print("SMTP Server shutting down...")
+        server.close()
