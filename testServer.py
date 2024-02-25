2cdc99af956ce84e5d649b0e391916737399b284
xutongxi <49649765+xutongxi@users.noreply.github.com>
2024-02-25 08:48:55 +0100

    change ip

--- testServer.py
+++ testServer.py
@@ -1,6 +1,6 @@
 from SMTPserver import SMTPServer
 
-host = '192.168.0.184'  # 示例IP地址；根据需要更改
+host = '163.123.183.116'  # 示例IP地址；根据需要更改
 port = 25  # 示例端口；根据需要更改
 smtp_server = SMTPServer(host=host, port=port)
 smtp_server.start_server()
